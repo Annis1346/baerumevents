0aeeb258c7e6b33425703252b41fcc25e638ecfb
snobjorn <asbjorn@snobjorn.no>
2023-02-13 19:58:07 +0100

    styling

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
       white: colors.white,
       slate: colors.slate,
       current: "currentColor",
+      pink: "#fac8c9",
+      offwhite: "#f1f0f0",
+      link: "#80060A",
     },
   },
 };
